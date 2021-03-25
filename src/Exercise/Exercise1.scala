82c034d65a0a8138b77e7eba8b94ea2539c7b6ac
Simone Luzi <simone.luzi@studio.unibo.it>
2021-03-25 19:58:14 +0100

    refactored ex1

--- src/Exercise/Exercise1.scala
+++ src/Exercise/Exercise1.scala
@@ -5,7 +5,7 @@ import u04lab.code.Complex
 object Exercise1 {
 
   case class ComplexImpl(override val re: Double,
-                    override val im: Double) extends Complex {
+                         override val im: Double) extends Complex {
 
     override def +(c: Complex): Complex = Complex(re+c.re, im+c.im)
 
--- test/TestExercise/TestExercise1.scala
+++ test/TestExercise/TestExercise1.scala
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.Assertions._
 class TestExercise1 {
 
   @Test def testComplexImpl(): Unit = {
-    val n1 = ComplexImpl(2, 3)
-    val n2 = ComplexImpl(3, 4)
+    val n1 = Complex(2, 3)
+    val n2 = Complex(3, 4)
     println(n1)
     assertTrue(n1==ComplexImpl(2,3))
     assertEquals(Complex(5,7), n1+n2)
